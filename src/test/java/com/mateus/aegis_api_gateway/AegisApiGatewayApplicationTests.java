6254c42856b2df7ddecbb5b1165a44aacfe6313b
mateus <mateus.magalhaes@hotmail.com.br>
2026-07-21 17:55:05 -0300

    api gateway config

---
+++ src/test/java/com/mateus/aegis_api_gateway/AegisApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.aegis_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
